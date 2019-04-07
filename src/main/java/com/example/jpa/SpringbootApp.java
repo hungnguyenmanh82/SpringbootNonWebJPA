0b2e85ef551aed1a5fa9c6e7884ccfd99e13f2f6
DESKTOP-LBOHS1J\hungnm2 <hung.nguyen@netvisiontel.com>
2019-04-07 12:15:46 +0700

    update

--- src/main/java/com/example/jpa/SpringbootApp.java
+++ src/main/java/com/example/jpa/SpringbootApp.java
@@ -15,146 +15,16 @@ import com.example.jpa.entity.Department;
 import com.example.jpa.entity.Employee;
 
 @SpringBootApplication
-public class SpringbootApp implements CommandLineRunner {
+public class SpringbootApp  {
 
-
+	/**
+	 * config the same web-application
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootApp.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-
-		//		testInsert1();
-		//		testInsert2();
-		//		testInsert3();
-
-		//		testSelect1();
-		//		testSelect2();
-		//		testSelect3();
-
-		//		testSelect4();
-		//		testSelect5();
-				testSelect6();
-//		testSelect7();
-
-	}
-
-	//================================================================================
-	//
-	//===============================================================================
-	@Autowired //refer to singleton PersistInsertDAO
-	PersistInsertDAO  persistInsertDAO;
-	public void testInsert1(){
-		System.out.println("************* Start testInsert1 ************************");
-		Department dep = new Department(986,"information","Hanoi");
-		persistInsertDAO.insertDepartment(dep);
-
-		System.out.println("OK: *************** PersistInsertDAO");
+		
+		//see Non-Webapp.java implement CommandLineRunner.class
 	}
 
 
-	//================================================================================
-	//
-	//===============================================================================
-	@Autowired
-	NativeInsertDAO nativeInsertDAO;
-	// chạy 2 lần để test exception => vi duplicate Id
-	public void testInsert2(){
-		System.out.println("************* Start testInsert2 ************************");
-		Department dep = new Department(999,"NativeSQL","SonTay");
-		nativeInsertDAO.insertDepartment1(dep);
-
-		System.out.println("OK: *************** NativeInsertDAO");
-	}
-
-
-	public void testInsert3(){
-		System.out.println("************* Start testInsert3 ************************");
-		Department dep = new Department(789,"@Transactional","Bavi");
-		nativeInsertDAO.insertDepartment2(dep);
-
-		System.out.println("OK: 333 *************** NativeInsertDAO");
-	}
-
-	//================================================================================
-	//
-	//===============================================================================
-	@Autowired //refer to singleton PersistInsertDAO
-	NativeSelectDAO nativeSelectDAO;  
-	public void testSelect1(){
-		System.out.println("************* Start testSelect1 ************************");
-		Department dep = nativeSelectDAO.findDepartmentById1(20);
-
-		System.out.println("OK: *******************************");
-		System.out.println("  depId=" + dep.getDeptId());
-		System.out.println("  depName=" + dep.getDeptName());
-	}
-
-	public void testSelect2(){
-		System.out.println("************* Start testSelect2 ************************");
-		Department dep = nativeSelectDAO.findDepartmentById2(20);
-
-		System.out.println("OK: *******************************");
-		System.out.println("   depId=" + dep.getDeptId());
-		System.out.println("   depName=" + dep.getDeptName());
-	}
-
-	public void testSelect3(){
-		System.out.println("************* Start testSelect3 ************************");
-		List<Department> deps = nativeSelectDAO.findAllDepartment();
-
-		for(Department dep: deps){
-			System.out.println("OK: *******************************");
-			System.out.println("     depId=" + dep.getDeptId());
-			System.out.println("     depName=" + dep.getDeptName());
-		}
-	}
-
-	//================================================================================
-	//
-	//===============================================================================
-	@Autowired //refer to singleton PersistInsertDAO
-	SelectDAO selectDAO;
-	public void testSelect4(){
-		System.out.println("************* Start testSelect4 ************************");
-		List<Employee> emps = selectDAO.findAllEmployee();
-
-		for(Employee emp: emps){
-			System.out.println("OK: *******************************");
-			System.out.println("     empId=" + emp.getEmpId());
-			System.out.println("     empName=" + emp.getEmpName());
-		}
-	}
-
-	public void testSelect5(){
-		System.out.println("************* Start testSelect5 ************************");
-		Department dep = selectDAO.findDepartmentById(20);
-
-		System.out.println("OK: *******************************");
-		System.out.println("   depId=" + dep.getDeptId());
-		System.out.println("   depName=" + dep.getDeptName());
-	}
-
-	public void testSelect6(){
-		System.out.println("************* Start testSelect6 ************************");
-		List<Employee> emps = selectDAO.findEmployeeByDepNo("D10");
-
-		for(Employee emp: emps){
-			System.out.println("OK: *******************************");
-			System.out.println("     empId=" + emp.getEmpId());
-			System.out.println("     empName=" + emp.getEmpName());
-			System.out.println("     DepNo=" + emp.getDepartment().getDeptNo());
-		}
-	}
-
-	public void testSelect7(){
-		System.out.println("************* Start testSelect7 ************************");
-		Employee emp = selectDAO.findEmployeeById(7369);
-
-		System.out.println("OK: *******************************");
-		System.out.println("     empId=" + emp.getEmpId());
-		System.out.println("     empName=" + emp.getEmpName());
-		System.out.println("     DepNo=" + emp.getDepartment().getDeptNo());
-	}
 }
